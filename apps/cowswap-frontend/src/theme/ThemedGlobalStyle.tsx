967af352480b2de5f1b01661364a588604b30c32
Dani Gámez Franco <gmzcodes@outlook.com>
2026-08-13 15:59:12 +0200

    feat: add button global disabled styles

--- apps/cowswap-frontend/src/theme/ThemedGlobalStyle.tsx
+++ apps/cowswap-frontend/src/theme/ThemedGlobalStyle.tsx
@@ -34,7 +34,7 @@ export const ThemedGlobalStyle = createGlobalStyle`
   }
 
   button,
-  input,
+  input:not([type='range'], [type='checkbox'], [type='radio']),
   textarea,
   select {
     border: none;
@@ -44,11 +44,16 @@ export const ThemedGlobalStyle = createGlobalStyle`
     outline: none;
     font: inherit;
     color: inherit;
+    appearance: none;
   }
 
   button {
     user-select: none;
     cursor: pointer;
+
+    &:disabled {
+      cursor: not-allowed;
+    }
   }
 
   html {
